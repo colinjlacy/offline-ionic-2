805dd2c9d1dc47749b6e2c693bc3813caf228900
colinjlacy <colin@webcake.co>
2015-12-29 12:26:19 -0600

    [create-template] builds a form for creating a list item with a submit that logs form values and an inactive image capture button

--- app/pages/create/create.page.ts
+++ app/pages/create/create.page.ts
@@ -12,4 +12,15 @@ export class Create {
 
     public title: string;
     public icon: string;
+
+    public item: {description: string, location: string} = {};
+    public options: Array = [
+        {value: 'here', label: 'Current location'},
+        {value: 'home', label: 'Home'},
+        {value: 'work', label: 'Work'}
+    ];
+
+    public save() {
+        console.log(this.item);
+    }
 }
