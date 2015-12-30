81389d4f3ea722997f56357f8809c005761a5bdc
colinjlacy <colin@webcake.co>
2015-12-29 18:51:28 -0600

    [data-store] taps into the ionic localstorage to set an initial list on load and revises the list service to pull from localstorage

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
-import {App, Platform} from 'ionic-framework/ionic';
+import {App, Platform, Storage, LocalStorage} from 'ionic-framework/ionic';
+import {LIST_ITEMS} from './data/list-item.data';
 
 import {List} from './pages/list/list.page';
 import {Create} from './pages/create/create.page';
@@ -6,7 +7,7 @@ import {Edit} from './pages/edit/edit.page';
 
 
 @App({
-    templateUrl: 'build/app.html'
+    templateUrl: 'build/app.html',
 })
 export class MyApp {
     constructor(platform: Platform) {
@@ -14,9 +15,12 @@ export class MyApp {
         // this tells the tabs component which Pages
         // should be each tab's root Page
         this.rootPage = List;
+        this.local = new Storage(LocalStorage);
+        this.local.set('data', JSON.stringify(LIST_ITEMS));
 
         platform.ready().then(() => {
             // Do any necessary cordova or native calls here now that the platform is ready
+
         });
     }
 }
--- app/data/list-item.service.ts
+++ app/data/list-item.service.ts
@@ -1,18 +1,20 @@
 /**
  * Created by colinjlacy on 12/29/15.
  */
-import {Injectable} from 'angular2/core'
-
-import {LIST_ITEMS} from './list-item.data';
+import {Injectable} from 'angular2/core';
+import {Storage, LocalStorage} from 'ionic-framework/ionic';
 
 @Injectable()
 
 export class ListItemService {
+    constructor() {
+        this.local = new Storage(LocalStorage);
+    }
     getItems() {
-        return Promise.resolve(LIST_ITEMS);
+        return this.local.get('data').then(data => JSON.parse(data));
     }
 
-    getItem(index) {
-        return Promise.resolve(LIST_ITEMS[index]);
+    getItem(id) {
+        return this.local.get('data').then(data => data.find(x => x.id === id));
     }
 }
